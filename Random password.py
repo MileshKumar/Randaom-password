de7f28a48bf6d8040f96e294b477e204e14ec0c8
MileshKumar <94909567+MileshKumar@users.noreply.github.com>
2021-12-05 22:18:57 +0530

    Update Random password.py

--- Random password.py
+++ Random password.py
@@ -10,3 +10,21 @@ generate = "".join(random.sample(password,pass_len))
 
 #then print
 print(generate)
+
+#Second Method
+#Another wey to generate random passwords:
+import random
+#User input from password lenght
+pass_len = int(input("Enter Password Of Lenght : "))
+
+# use to different - different types
+upperstr = "QWERTYUIOPASDFGHJKLZXCVBNM"
+lowerstr = "qwertyuioplkjhgfdsazxcvbnm"
+numbers = "0123456789"
+symbol = "~!@#$%^&*()_+|></"
+password = upperstr+lowerstr+numbers+symbol
+#this is the formula to generate the password
+generate = "".join(random.sample(password,pass_len))
+
+#then print
+print(generate)
